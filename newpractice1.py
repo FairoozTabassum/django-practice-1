12b70e43ebf5f6b3d80201d3df8d4ca54b527edc
Fairooz Tabassum <ttabassuum@gmail.com>
2026-05-23 01:36:21 +0600

    feat: add login system (Feature X)

--- newpractice1.py
+++ newpractice1.py
@@ -1,3 +1,8 @@
 
 print('Change from main branch')
+<<<<<<< HEAD
 print('Change from branch A')Feature X: Login System Added
+=======
+print('Change from branch A')Feature Y: Dashboard UI Added
+>>>>>>> feature-y
+Feature X: Login System Added
