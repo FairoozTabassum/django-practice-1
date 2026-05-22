094f1f02682eea5a2fe8d4219fe06d368ac86e8d
Fairooz Tabassum <ttabassuum@gmail.com>
2026-05-23 01:08:03 +0600

    Squashed commit: applied stashed changes practice

--- newpractice1.py
+++ newpractice1.py
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 def git3():
     print("Main Branch")
+=======
+def git2():
+    print("Feature Branch")
+>>>>>>> 859b786 (Feature branch update)
 
-git3()  
+git3()  stash practice
+# amend practice
